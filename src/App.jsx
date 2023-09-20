0aa464294c7ecc6c921fa1283ee72eabc47f4ee9
Lawrence Caulker <locaulker@gmail.com>
2023-09-20 18:43:21 -0400

    implemented category menu items

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,24 @@ import menu from "./data"
 
 import Title from "./Title"
 import Menu from "./Menu"
+import Categories from "./Categories"
 
 // const tempCategories = menu.map(item => item.category)
 // const tempSet = new Set(tempCategories)
 // const tempItems = ["all", ...tempSet]
 // console.log(tempItems)
 
-const allCategories = ["all", new Set(menu.map(item => item.category))]
+const allCategories = ["all", ...new Set(menu.map(item => item.category))]
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu)
   const [categories, setCategories] = useState(allCategories)
-  console.log(categories)
 
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu" />
+        <Categories categories={categories} />
         <Menu items={menuItems} />
       </section>
     </main>
